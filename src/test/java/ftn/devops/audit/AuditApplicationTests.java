4af7f0cb8fa1349d8c4642cbfb66fe8013ebb5a9
Nadja Gvozdenac <gvozdenacn@gmail.com>
2024-05-28 22:47:00 +0200

    feat: Initialise spring project.

---
+++ src/test/java/ftn/devops/audit/AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.devops.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
