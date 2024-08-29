c6214cae0b09031e61b39997e78b90732b3e6944
Dusan Brkic <dusanbrk@gmail.com>
2024-08-29 23:49:57 +0200

    ci: setup verification and deployment

--- src/main/java/ftn/devops/audit/config/CassandraConfig.java
+++ src/main/java/ftn/devops/audit/config/CassandraConfig.java
@@ -1,7 +1,6 @@
 package ftn.devops.audit.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
